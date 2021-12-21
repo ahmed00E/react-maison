3c119a1c1d463ee1d3e6dfa25a6cd460baadda6d
ahmed00E <afrh20066@gmail.com>
2021-12-22 00:44:05 +0100

    first commit

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,9 @@
+import { plantList } from '../datas/plantList'
+import '../styles/Banner.css'
+ 
+
+function ShoppingList() {
+	return <h3 className='lss'> topClassic Ahmed</h3>
+}
+
+export default ShoppingList
